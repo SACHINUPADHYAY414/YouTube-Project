a042006380927b3c162fb384bac6134e7b2573cd
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2024-07-15 13:27:43 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,18 +1,33 @@
+// import axios from 'axios';
+
+// export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+// export const options = {
+//   params: {
+//     maxResults: 50,
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+//     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+//     // 'X-RapidAPI-Host': 'youtube138.p.rapidapi.com',
+//   },
+// };
+
+// export const axiosGetReq = async (url) => {
+//   const res = await axios.get(`${BASE_URL}/${url}`, options);
+//   return res.data;
+// };
 import axios from 'axios';
 
+export const BASE_URL = 'https://youtube138.p.rapidapi.com';
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
-export const options = {
+
+const options = {
   params: {
     maxResults: 50,
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube138.p.rapidapi.com',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-    // 'X-RapidAPI-Host': 'youtube138.p.rapidapi.com',
   },
-};
-
-export const axiosGetReq = async (url) => {
-  const res = await axios.get(`${BASE_URL}/${url}`, options);
-  return res.data;
 };
