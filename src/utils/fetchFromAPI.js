7a9560157ce955de6310caf916828df68351c8d6
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2024-07-15 13:21:50 +0530

    Update fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,8 +7,8 @@ export const options = {
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    // 'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-    'X-RapidAPI-Host': 'youtube138.p.rapidapi.com',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    // 'X-RapidAPI-Host': 'youtube138.p.rapidapi.com',
   },
 };
 
