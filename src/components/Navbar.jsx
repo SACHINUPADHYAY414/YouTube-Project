d1d218e77efd775bd29035b3cc009acfe49e911c
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2024-07-15 14:55:03 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,23 @@
-import { Stack } from "@mui/material";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-import { logo } from "../utils/constants";
-import { SearchBar } from "./";
-
+import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
 const Navbar = () => (
-    <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}
+  >
+    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
       <img src={logo} alt="logo" height={45} />
-        <h2>Movies4u</h2>
     </Link>
     <SearchBar />
   </Stack>
 );
 
-export default Navbar;
+
+
+export default Navbar
