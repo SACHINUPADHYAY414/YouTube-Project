d5de51c3050e85677c4bd745b7d1d76231aecfbd
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2025-03-16 12:59:59 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,24 @@ import { Link } from 'react-router-dom';
 
 import { logo } from '../utils/constants';
 import SearchBar from './SearchBar';
+
 const Navbar = () => (
   <Stack
     direction="row"
     alignItems="center"
     p={2}
-    sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}
+    sx={{ 
+      position: 'sticky', 
+      background: '#000', 
+      top: 0, 
+      justifyContent: 'center' // Center items in the Navbar
+    }}
   >
-    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+    <Link to="/" style={{ display: 'flex', alignItems: 'center', marginRight: 'auto' }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
     <SearchBar />
   </Stack>
 );
 
-
-
-export default Navbar
+export default Navbar;
